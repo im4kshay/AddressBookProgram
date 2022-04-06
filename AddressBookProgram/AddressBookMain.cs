7015ce931aaadbd54657a7dc96d5756f9787f583
im4kshay <akshaysayre7@gmail.com>
2022-04-07 02:29:54 +0530

    Ability to edit existing contact person using their name

--- AddressBookProgram/AddressBookMain.cs
+++ AddressBookProgram/AddressBookMain.cs
@@ -8,21 +8,11 @@ namespace AddressBookProgram
 {
     class AddressBookMain
     {
+        public static List<Contacts> listCon = new List<Contacts>();
         static void Main()
         {
             Console.WriteLine("Welcome to Address Book Program");
-            Console.WriteLine("Enter 1 to Add Contact");
-            int enterKey = Convert.ToInt32(Console.ReadLine());
-            switch (enterKey)
-            {
-                case 1:
-                    Contacts.AddContacts();
-                    break;
-                default:
-                    Console.WriteLine("Wrong Key. Try once more\n");
-                    AddressBookMain.Main();
-                    break;
-            }
+            Calling.CallingAddressBook();
         }
 
     }
--- AddressBookProgram/Contacts.cs
+++ AddressBookProgram/Contacts.cs
@@ -14,9 +14,9 @@ namespace AddressBookProgram
         public string city;
         public string state;
         public int zip;
-        public long phoneNo;
+        public int phoneNo;
         public string email;
-        public Contacts(string firstName, string secondName, string address, string city, string state, int zip, long phoneNo, string email)
+        public Contacts(string firstName, string secondName, string address, string city, string state, int zip, int phoneNo, string email)
         {
             this.firstName = firstName;
             this.secondName = secondName;
@@ -45,9 +45,43 @@ namespace AddressBookProgram
             int phoneNumber = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter Email Id");
             string personEmail = Console.ReadLine();
-            Contacts conObj = new Contacts(fName, sName, personAddress, personCity, personState, personZip, phoneNumber, personEmail);
+            string name = fName;
+            Contacts objCon = new Contacts(fName, sName, personAddress, personCity, personState, personZip, phoneNumber, personEmail);
+            AddressBookMain.listCon.Add(objCon);
             Console.WriteLine("Contact has been Added");
-            Console.ReadLine();
+        }
+        public static void EditContact()
+        {
+            Console.WriteLine("Enter First Name");
+            string fName = Console.ReadLine();
+            Console.WriteLine("Enter Second Name");
+            string sName = Console.ReadLine();
+            bool personFound = false;
+            foreach (Contacts item in AddressBookMain.listCon)
+            {
+                if (((item.firstName).ToLower() == fName.ToLower()) && ((item.secondName).ToLower() == sName.ToLower()))
+                {
+                    Console.WriteLine("Enter new Address");
+                    item.address = Console.ReadLine();
+                    Console.WriteLine("Enter new City");
+                    item.city = Console.ReadLine();
+                    Console.WriteLine("Enter new State");
+                    item.state = Console.ReadLine();
+                    Console.WriteLine("Enter new Address");
+                    item.zip = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter new Phone Number");
+                    item.phoneNo = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter new Email");
+                    item.email = Console.ReadLine();
+                    personFound = true;
+                    Console.WriteLine("Details have been updated");
+                }
+            }
+            if (personFound == false)
+            {
+                Console.WriteLine("Person not found");
+            }
+
         }
     }
 }
