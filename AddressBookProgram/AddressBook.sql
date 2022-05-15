f4e655e0e310b36671737f0e0f5ecf5e5ab60966
im4kshay <akshaysayre7@gmail.com>
2022-05-15 13:43:27 +0530

    Method to Add new contact into DB

--- AddressBookProgram/AddressBook.sql
+++ AddressBookProgram/AddressBook.sql
@@ -78,3 +78,19 @@ update AddressBook set Zip=@Zip where FirstName=@FirstName;
 alter table AddressBook add Date_Added date not null default getdate();
 update AddressBook set Date_Added = '2019-02-10' where FirstName='Manali';
 update AddressBook set Date_Added = '2019-02-10' where FirstName='Anuj';
+
+--addnew contact 
+create procedure spAddContact
+(
+@FirstName varchar(100),
+@LastName varchar(100),
+@Address varchar(100),
+@City varchar(100),
+@State varchar(100),
+@Zip bigint,
+@PhoneNumber bigint,
+@Email varchar(100)
+)
+as
+	insert into AddressBook (FirstName, LastName, Address, City, State, Zip, PhoneNumber, Email) 
+	values(@FirstName, @LastName, @Address, @City, @State, @Zip, @PhoneNumber, @Email);
